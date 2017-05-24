2e9ad5be889ee9fed73a0da154830aed6162e7f8
Martin B <mbompr@gmail.com>
2017-05-24 16:32:22 +0200

    refactored benchmark

--- benchmarks/hawkes_likelihood.R
+++ benchmarks/hawkes_likelihood.R
@@ -5,90 +5,48 @@
 library(hawkes)
 library(data.table)
 
-get_beta <- function(n_nodes) {
-    if (n_nodes == 1) {
-        return(1.5)
-    }
-    else if (n_nodes == 2) {
-        return(c(2., 2.))
-    }
-    else if (n_nodes == 4) {
-        return(c(0.5, 0.5, 0.5, 0.5))
-    }
-}
-n_nodes_sample <- c(1, 2, 4)
-end_times <- c(10000, 20000, 50000, 100000, 200000, 500000, 1000000)
+source("hawkes_data.R")
 
+n_nodes_sample <- c(1, 2)#, 4, 16)
+end_times <- c(10000, 20000)#, 50000, 100000, 200000, 500000, 1000000)
 
 
 for (n_nodes in n_nodes_sample) {
+
+    beta <- get_beta(n_nodes)
+    betas <- rep(beta, n_nodes)
+    test_coeffs <- get_test_coeffs(n_nodes)
+    test_baseline_list = test_coeffs[[1]]
+    test_alpha_list = test_coeffs[[2]]
+
     for (end_time in end_times) {
-
-        SIMULATION_FILE = sprintf("hawkes_data/hawkes_simulation_n_nodes_%i_end_time_%i.txt",
-                                  n_nodes, end_time)
-        # print(SIMULATION_FILE)
-
-        TEST_COEFFS_FILE = sprintf("hawkes_data/hawkes_test_coeffs_n_nodes_%i.txt",
-                                   n_nodes)
-
-        history <- list()
-        con <- file(SIMULATION_FILE, open='r')
-        node <- 1
-        for (line in readLines(con)) {
-            node_data <- fread(paste(line, "\n"))
-            history[[node]] <- as.matrix(node_data)
-            node = node + 1;
-        }
-        closeAllConnections()
-        if (n_nodes == 1) {
-            history <- history[[1]]
-        }
+        history <- get_simulation(n_nodes, end_time)
 
         # Multivariate Hawkes process
-        betas <- get_beta(n_nodes)
-        beta = mean(betas)
-
-        test_coeffs = unname(as.matrix(read.table(TEST_COEFFS_FILE)))
-
-        compute_likelihoods <- function(test_coeffs) {
-            for (i in 1:nrow(test_coeffs)) {
-                if (n_nodes == 1) {
-                    lambda0 <- test_coeffs[i, 1]
-                    alpha <- test_coeffs[i, 2] * beta
-                }
-                else {
-                    lambda0 = test_coeffs[i, 0:n_nodes]
-                    alpha = matrix(
-                        test_coeffs[i, (n_nodes+1): (n_nodes * (1 + n_nodes))] * beta,
-                        byrow = TRUE, nrow = n_nodes)
-                }
+        compute_likelihoods <- function(test_baseline_list, test_alpha_list) {
+            n_tries = length(test_baseline_list)
+            for (i in 1:n_tries) {
+                lambda0 <- test_baseline_list[[i]]
+                alpha <- test_alpha_list[[i]]
                 likelihoodHawkes(lambda0, alpha, betas, history)
             }
         }
-        compute_likelihoods(test_coeffs)
 
-        benchmark <- system.time(compute_likelihoods(test_coeffs))
-        average_time <- benchmark[["elapsed"]] / nrow(test_coeffs)
-        #print(sprintf('Average time for one likelihood computation %.6f',
-        #              average_time))
+        n_tries = length(test_baseline_list)
+        benchmark <- system.time(compute_likelihoods(test_baseline_list, test_alpha_list))
+        average_time <- benchmark[["elapsed"]] / n_tries
         n_events <- Reduce("+", lapply(history, length))
         cat(sprintf('likelihood,hawkes R,%i,%i,%.5f\n',
                     n_nodes, n_events, average_time))
         cat(sprintf('first likelihood,hawkes R,%i,%i,%.5f\n',
                     n_nodes, n_events, average_time))
 
-        # test first coeff
-        if (n_nodes == 1) {
-            lambda0 <- test_coeffs[1, 1]
-            alpha <- test_coeffs[1, 2] * beta
+        # end_time 100 is used for debug
+        if (end_time == 100) {
+            lambda0 <- test_baseline_list[[1]]
+            alpha <- test_alpha_list[[1]]
+            l <- likelihoodHawkes(lambda0, alpha, betas, history)
+            print(sprintf('Negative loglikelihood value on first test coeff %.6f', l))
         }
-        else {
-            lambda0 = test_coeffs[1, 0:n_nodes]
-            alpha = matrix(
-                test_coeffs[1, (n_nodes+1): (n_nodes * (1 + n_nodes))] * beta,
-                byrow = TRUE, nrow = n_nodes)
-        }
-        l <- likelihoodHawkes(lambda0, alpha, betas, history)
-        # print(sprintf('Negative loglikelihood value on first test coeff %.6f', l))
     }
 }
--- benchmarks/hawkes_likelihood.py
+++ benchmarks/hawkes_likelihood.py
@@ -1,97 +1,51 @@
-import os
 import time
 
-import numpy as np
+from benchmarks.create_hawkes_data import get_test_coeffs, get_decay, \
+    get_simulation
 
-from benchmarks.hawkes_simulation import get_parameters
-from tick.simulation import SimuHawkesExpKernels
 from tick.optim.model import ModelHawkesFixedExpKernLogLik
 
-SIMULATION_FILE = "hawkes_data/hawkes_simulation_n_nodes_{}_end_time_{}.txt"
-TEST_COEFFS_FILE = "hawkes_data/hawkes_test_coeffs_n_nodes_{}.txt"
+n_nodes_sample = [1, 2, 4, 16]
+end_times = [10000, 20000]#, 50000, 100000, 200000, 500000, 1000000]
+n_threads = [1, 4, 16]
 
+for n_thread in n_threads:
+    for n_nodes in n_nodes_sample:
 
-def simulate_hawkes_data(n_nodes, end_time):
-    """Generates benchmarks data that will be used by all tests
-    """
-    decays, baseline, adjacency = get_parameters(n_nodes)
+        test_coeffs = get_test_coeffs(n_nodes)
+        decays = get_decay(n_nodes)
 
-    hawkes_exp_kernels = SimuHawkesExpKernels(
-        adjacency=adjacency, decays=decays, baseline=baseline,
-        end_time=end_time, seed=1039, verbose=False)
+        for end_time in end_times:
+            timestamps = get_simulation(n_nodes, end_time)
 
-    hawkes_exp_kernels.simulate()
+            n_events = sum(map(len, timestamps))
 
-    if not os.path.exists("hawkes_data"):
-        os.mkdir("hawkes_data")
+            n_tries_first_likelihood = 10
+            start_time = time.clock()
+            for _ in range(n_tries_first_likelihood):
+                model = ModelHawkesFixedExpKernLogLik(decays,
+                                                      n_threads=n_thread)
+                model.fit(timestamps)
+                model.loss(test_coeffs[0])
+            first_likelihood_time = ((time.clock() - start_time) /
+                                     n_tries_first_likelihood)
 
-    with open(SIMULATION_FILE.format(n_nodes, end_time),
-              "ba") as sim_by_node_file:
-        for node in range(hawkes_exp_kernels.n_nodes):
-            np.savetxt(sim_by_node_file, hawkes_exp_kernels.timestamps[node],
-                       newline=' ')
-            sim_by_node_file.write(b'\n')
+            start_time = time.clock()
+            for test_coeff in test_coeffs:
+                loss = model.loss(test_coeff)
+            average_compute_likelihood_time = (time.clock() - start_time) / len(
+                test_coeffs)
 
-    # print("Saved simulation with {} nodes and {} total jumps\n"
-    #       .format(n_nodes, hawkes_exp_kernels.n_total_jumps))
+            # end_time 100 is used for debug
+            if end_time != 100:
+                print("likelihood,tick {},{},{},{:.6f}"
+                      .format(n_thread, n_nodes, n_events,
+                              average_compute_likelihood_time))
 
-
-def simulate_test_coeffs(n_nodes):
-    np.random.seed(3209)
-
-    n_test_coeffs = 10
-    test_coeffs = np.random.rand(n_test_coeffs, n_nodes * (1 + n_nodes))
-    np.savetxt(TEST_COEFFS_FILE.format(n_nodes), test_coeffs)
-
-
-n_nodes_sample = [1, 2, 4]
-end_times = [10000, 20000, 50000, 100000, 200000, 500000, 1000000]
-
-for n_nodes in n_nodes_sample:
-    simulate_test_coeffs(n_nodes)
-    test_coeffs = np.loadtxt(TEST_COEFFS_FILE.format(n_nodes))
-    decays, _, _ = get_parameters(n_nodes)
-
-    for end_time in end_times:
-        data_exists = os.path.exists(SIMULATION_FILE.format(n_nodes, end_time))
-        if not data_exists:
-            simulate_hawkes_data(n_nodes, end_time)
-
-        timestamps = []
-        with open(SIMULATION_FILE.format(n_nodes, end_time),
-                  "r") as sim_by_node_file:
-            for node in range(n_nodes):
-                timestamps.append(np.fromstring(sim_by_node_file.readline(),
-                                                sep=' '))
-
-        n_events = sum(map(len, timestamps))
-
-        n_tries_first_likelihood = 10
-        start_time = time.clock()
-        for _ in range(n_tries_first_likelihood):
-            model = ModelHawkesFixedExpKernLogLik(np.mean(decays))
-            model.fit(timestamps)
-            model.loss(test_coeffs[0])
-        first_likelihood_time = (time.clock() - start_time) / n_tries_first_likelihood
-
-        start_time = time.clock()
-        for test_coeff in test_coeffs:
-            loss = model.loss(test_coeff)
-        average_compute_likelihood_time = (time.clock() - start_time) / len(test_coeffs)
-
-        if False:
-            print('Time needed for first likelihood {:.6f}'
-                  .format(first_likelihood_time))
-
-            print('Average time to compute likelihood {:.6f}'
-                  .format(average_compute_likelihood_time))
-
-            print("Negative loglikelihood value on first test coeff {:.6f}"
-                  .format(model.loss(test_coeffs[0]) * n_events))
-
-        else:
-            print("likelihood,tick,{},{},{:.6f}"
-                  .format(n_nodes, n_events, average_compute_likelihood_time))
-    
-            print("first likelihood,tick,{},{},{:.6f}"
-                  .format(n_nodes, n_events, first_likelihood_time))
+                print("first likelihood,tick {},{},{},{:.6f}"
+                      .format(n_thread, n_nodes, n_events,
+                              first_likelihood_time))
+            else:
+                print("Negative loglikelihood value on first test coeff {:.6f}"
+                      .format(model.loss(test_coeffs[0]) * n_events))
+                print()
