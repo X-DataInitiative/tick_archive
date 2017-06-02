4e590ac66acf5e6b443976275fc7afd0ae7d11ec
Martin B <mbompr@gmail.com>
2017-06-02 16:26:17 +0200

    forget about parallel simulations

--- benchmarks/hawkes_simulation.py
+++ benchmarks/hawkes_simulation.py
@@ -9,36 +9,32 @@ if __name__ == "__main__":
 
     n_nodes_sample = [1, 2, 4, 16]
     end_times = [1000, 2000, 5000, 10000, 20000, 50000, 100000]
-    n_threads = [1, 4, 16]
 
-    for n_thread in n_threads:
-        for n_nodes in n_nodes_sample:
-            decays, baseline, adjacency = get_parameters(n_nodes)
+    for n_nodes in n_nodes_sample:
+        decays, baseline, adjacency = get_parameters(n_nodes)
+
+        if False:
+            print("\nn nodes", n_nodes)
+            print('baseline\n', baseline)
+            print('decays\n', decays)
+            print('adjacency\n', adjacency)
+            print('adjacency * decays\n', adjacency * decays)
+
+        for end_time in end_times:
+            hawkes_exp_kernels = SimuHawkesExpKernels(
+                adjacency=adjacency, decays=decays, baseline=baseline,
+                end_time=end_time, seed=1039, verbose=False)
+
+            start_time = time.time()
+            for _ in range(n_simulations):
+                hawkes_exp_kernels.reset()
+                hawkes_exp_kernels.simulate()
+            simulation_time = time.time() - start_time
 
             if False:
-                print("\nn nodes", n_nodes)
-                print('baseline\n', baseline)
-                print('decays\n', decays)
-                print('adjacency\n', adjacency)
-                print('adjacency * decays\n', adjacency * decays)
-
-            for end_time in end_times:
-                hawkes_exp_kernels = SimuHawkesExpKernels(
-                    adjacency=adjacency, decays=decays, baseline=baseline,
-                    end_time=end_time, seed=1039, verbose=False)
-
-                multi = SimuHawkesMulti(hawkes_exp_kernels,
-                                        n_simulations=n_simulations,
-                                        n_threads=n_thread)
-
-                start_time = time.time()
-                multi.simulate()
-                simulation_time = time.time() - start_time
-
-                if False:
-                    print("Simulating {} events costs {} secs"
-                          .format(sum(multi.n_total_jumps), simulation_time))
-                else:
-                    print("simulation,tick {},{},{},{:.6f}"
-                          .format(n_thread, n_nodes, sum(multi.n_total_jumps),
-                                  simulation_time))
+                print("Simulating {} events costs {} secs"
+                      .format(hawkes_exp_kernels.n_total_jumps, simulation_time))
+            else:
+                print("simulation,tick 1,{},{},{:.6f}"
+                      .format(n_nodes, hawkes_exp_kernels.n_total_jumps,
+                              simulation_time))
