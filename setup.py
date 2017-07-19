ericustc/tick

TICK-385 Added O3 compilation flags and optional fast-math

setup.py
@@ -37,6 +37,10 @@ from scipy.sparse import csr_matrix
 
 debug_flags = ['DEBUG_COSTLY_THROW']
 
+# If true, add compilation flags to use fast (but maybe inaccurate) math
+# See https://gcc.gnu.org/wiki/FloatingPointMath
+use_fast_math = False
+
 # We need to understand what kind of ints are used by the sparse matrices of
 # scipy
 sparsearray = csr_matrix(
@@ -212,7 +216,11 @@ def create_extension(extension_name, module_dir,
                               '-I./tick/base/src/',
                               sparse_indices_flag,
                               '-std=c++11',
+                              '-O3',
                               ]
+
+    if use_fast_math:
+        min_extra_compile_args.append('-ffast-math')
 
     if extra_compile_args is None:
         extra_compile_args = min_extra_compile_args

X-DataInitiative/tick_archive

setup.py
@@ -71,6 +71,11 @@ if sys.platform == 'darwin':
     vars = sysconfig.get_config_vars()
     vars['LDSHARED'] = vars['LDSHARED'].replace('-bundle', '-dynamiclib')
 
+# If true, add compilation flags to use fast (but maybe inaccurate) math
+# See https://gcc.gnu.org/wiki/FloatingPointMath
+use_fast_math = True
+
+
 # We need to understand what kind of ints are used by the sparse matrices of
 # scipy
 sparsearray = csr_matrix(
@@ -244,7 +249,11 @@ def create_extension(extension_name, module_dir,
                               '-I./tick/base/src/',
                               sparse_indices_flag,
                               '-std=c++11',
+                              '-O3',
                               ]
+
+    if use_fast_math:
+        min_extra_compile_args.append('-ffast-math')
 
     if extra_compile_args is None:
         extra_compile_args = min_extra_compile_args