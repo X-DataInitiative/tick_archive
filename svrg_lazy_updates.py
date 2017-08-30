49517b0265aa0ea55c2febfc353106dd06e38a3d
Stephane Gaiffas <stephane.gaiffas@polytechnique.edu>
2017-08-30 13:16:23 +0200

    ...

--- svrg_lazy_updates.py
+++ svrg_lazy_updates.py
@@ -1,4 +1,3 @@
-
 # coding: utf-8
 
 # In[1]:
@@ -6,7 +5,6 @@
 import sys
 
 sys.path.append('/Users/stephane.gaiffas/Code/tick')
-
 
 import numpy as np
 
@@ -32,7 +30,7 @@ def simu_linreg(x, n, interc=None, std=1., corr=0.5, p_nnz=0.3):
 
     corr : float, default=0.5
         Correlation of the features matrix
-    """    
+    """
     d = x.shape[0]
     cov = toeplitz(corr ** np.arange(0, d))
     A = multivariate_normal(np.zeros(d), cov, size=n)
@@ -46,20 +44,20 @@ def simu_linreg(x, n, interc=None, std=1., corr=0.5, p_nnz=0.3):
         b += interc
     return A, b
 
+
 n = 100000
 d = 30
 
-idx = np.arange(1, d+1)
+idx = np.arange(1, d + 1)
 
 # Ground truth coefficients of the model
 x_truth = (-1) ** (idx - 1) * np.exp(-idx / 10.)
 
-A_dense, b = simu_linreg(x_truth, n, interc=-1., 
+A_dense, b = simu_linreg(x_truth, n, interc=-1.,
                          std=1., corr=0.8, p_nnz=0.05)
 A_spars = csr_matrix(A_dense)
 
 n, d = A_spars.shape
-
 
 # In[50]:
 
@@ -72,7 +70,6 @@ from time import time
 
 t1 = time()
 
-
 model_spars = ModelLinReg().fit(A_spars, b)
 model_dense = ModelLinReg().fit(A_dense, b)
 
@@ -81,8 +78,8 @@ proxs = [
     ProxL1(strength=1e-2, range=(0, d)),
     ProxTV(strength=1e-2, range=(0, d)),
     ProxElasticNet(strength=1e-2, ratio=0.9, range=(0, d)),
-    ProxSlope(strength=1e-3, fdr=0.6, range=(0, d)),
-    ProxGroupL1(strength=1e-2, blocks_start = [0, 5, 10],
+    # ProxSlope(strength=0., fdr=0.1),
+    ProxGroupL1(strength=1e-2, blocks_start=[0, 5, 10],
                 blocks_length=[5, 5, 8], range=(0, d)),
     ProxEquality(range=(0, d))
 ]
@@ -93,8 +90,8 @@ step = 1 / model_spars.get_lip_max()
 variance_reductions = ['last']
 # rand_types = ['unif', 'perm']
 rand_types = ['unif']
-delayed_updates = ['exact', 'proba']
-
+# delayed_updates = ['exact', 'proba']
+delayed_updates = ['exact']
 
 seed = 123
 solvers_dense = []
@@ -103,7 +100,6 @@ solutions = []
 titles = []
 tol = 0.
 max_iter = 30
-
 
 from itertools import product
 
@@ -116,8 +112,8 @@ for variance_reduction, rand_type, delayed_update, prox \
                   variance_reduction=variance_reduction,
                   rand_type=rand_type,
                   delayed_updates=delayed_update,
-                  seed=seed)\
-        .set_model(model_spars)\
+                  seed=seed) \
+        .set_model(model_spars) \
         .set_prox(prox)
     solvers_sparse.append(solver)
 
@@ -126,15 +122,14 @@ for variance_reduction, rand_type, delayed_update, prox \
                   variance_reduction=variance_reduction,
                   rand_type=rand_type,
                   delayed_updates=delayed_update,
-                  seed=seed)\
-        .set_model(model_dense)\
+                  seed=seed) \
+        .set_model(model_dense) \
         .set_prox(prox)
     solvers_dense.append(solver)
 
     title = "VR='" + variance_reduction + "'; RT='" + rand_type + "; UP='" \
             + delayed_update + "' " + "; prox=" + prox.name
     titles.append(title)
-
 
 for solver_sparse, solver_dense, title in zip(solvers_sparse, solvers_dense,
                                               titles):
